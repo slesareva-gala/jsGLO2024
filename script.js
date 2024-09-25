7fb4aedfbf2ec75069caebff0f9cb99b50871fd0
Галина Слесарева <galaslesareva@gmail.com>
2024-09-25 13:24:27 +0300

    add Задание 1

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Карточка товара",
+    screens = 2,
+    screenPrice = 10,
+    rollback = 0.02,
+    fullPrice = 20,
+    adaptive = true;
+
+alert("Доброе утро !");
+console.log('Чашечку кофе?');
