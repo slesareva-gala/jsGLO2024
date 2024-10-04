c640fa6d5fe1aa02391d7964529986c9d5b029b1
Галина Слесарева <galaslesareva@gmail.com>
2024-10-05 00:41:50 +0300

    add: задание 5

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 'use strict';
 
-let title = prompt("Как называется ваш проект?", "Сайт, Лендинг ... ")
-  || "Разработка сайта";
-let screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
-  || "Разнообразные";
-let screenPrice = parseInt(prompt("Сколько будет стоить данная работа?", 12000))
-  || 12000;
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt("Какой дополнительный тип услуги нужен?", "1. ")
-  || "не указан";
-let servicePrice1 = parseInt(prompt("Сколько это будет стоить?", 0))
-  || 0;
-let service2 = prompt("Какой дополнительный тип услуги нужен?", "2.")
-  || "не указан";
-let servicePrice2 = parseInt(prompt("Сколько это будет стоить?", 0))
-  || 0;
-
+let title;
+let screens;
+let screenPrice;
+let adaptive;
 let rollback = 10;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
+let service1;
+let service2;
 
-let allServicePrices, fullPrice, servicePercentPrice;
+const isNumber = function (str) {
+  return !isNaN(parseFloat(str)) && isFinite(str);
+}
 
-const getTitle = str => {
-  str = str.trim();
-  return str = str[0].toUpperCase() + str.slice(1).toLowerCase();
-};
+const rounded = (num, dec = 0) => +num.toFixed(dec);
 
-const getAllServicePrices = function (...prices) {
-  return prices.reduce((sum, n) => sum + n, 0);
+const toMoney = (str) => rounded(Math.abs(+str));
+
+const asking = function () {
+  title = prompt("Как называется ваш проект?", "Сайт, Лендинг ... ")
+    || "Разработка сайта";
+  screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные")
+    || "Разнообразные";
+
+  do {
+    screenPrice = prompt("Сколько будет стоить данная работа?", "0") || "0";
+  } while (!isNumber(screenPrice));
+  screenPrice = toMoney(screenPrice);
+
+  adaptive = confirm('Нужен ли адаптив на сайте?');
+}
+
+const getTitle = str => str.trim()[0].toUpperCase() + str.trim().slice(1).toLowerCase();
+
+const getAllServicePrices = function () {
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    const service = (prompt("Какой дополнительный тип услуги нужен ?", `услуга ${i + 1} `) || `услуга ${i + 1}`).trim();
+    let price;
+
+    if (i === 0) service1 = service;
+    else if (i === 1) service2 = service;
+
+    do {
+      price = prompt(`Сколько это будет стоить "${service}" ?`, "0") || "0";
+    } while (!isNumber(price));
+    sum += toMoney(price);
+  }
+  return sum;
 };
 
 function getFullPrice(n1, n2) {
@@ -42,18 +66,21 @@ const getRollbackMessage = (cost) =>
       : cost >= 0 ? "Скидка не предусмотрена"
         : "Что то пошло не так";
 
-const showTypeOf = function (variable) {
-  console.log(variable, typeof variable);
+const showTypeOf = function (varName, varSense) {
+  console.log(varName + ": ", varSense, typeof varSense);
 }
 
+asking();
 title = getTitle(title);
-allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice(screenPrice, allServicePrices);
 servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
+showTypeOf("title", title);
+showTypeOf("screenPrice", screenPrice);
+showTypeOf("allServicePrices", allServicePrices);
+showTypeOf("fullPrice", fullPrice);
+showTypeOf("adaptive", adaptive);
 
 console.log('Типы экранов: ', screens.toLowerCase().split(", "));
 console.log(getRollbackMessage(fullPrice));
